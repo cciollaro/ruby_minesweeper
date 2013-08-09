41b29f823b463fe648be1e83adc7d3390b896605
Chris Ciollaro <cciollaro@gmail.com>
2013-08-08 20:53:47 -0400

    ran into a huge snag - a bug in qtbindings is preventing right clicks. I hate getting segfaults in ruby. anyway there's not currently a flagging feature or full-fledged endgame, but this is probably my final commit. it's in playable condition.

--- minesweeper.rb
+++ minesweeper.rb
@@ -13,10 +13,21 @@ X = 10 #X bombs
 class Cell
 	attr_accessor :is_flipped, :is_flagged, :surrounding_mines, :x, :y
 	def initialize(mines, x, y)
-		@is_flipped = false
+		@is_flagged = @is_flipped = false
 		@surrounding_mines = mines
 		@x = x
 		@y = y
+	end
+	
+	def to_button
+		b = Qt::PushButton.new
+		b.set_text self.to_str
+		b.set_flat self.is_flipped
+		b.resize 40, 40
+		b.connect(SIGNAL :clicked) do
+			$g.process_input(@y, @x)
+		end
+		return b
 	end
 end
 
@@ -56,124 +67,100 @@ class EmptyCell < Cell
 	end
 end
 
-# get some user input
-def user_input
-	begin
-		puts 'do you want to guess or flag? (g)uess/(f)lag' 
-	end until (action = gets).match(/[gfs]/)
-	
-	print_board(true) if action =~ /s/
-	
-	puts 'which spot?'
-	print 'x: '
-	x = gets.to_i - 1
-	print 'y: '
-	y = gets.to_i - 1
-	return [action, x, y]
-end
-  
-#returns a bool: true means go to next turn, false means game over
-def process_input(action, x, y)
-	chosen_cell = @board[y][x]
-	if action =~ /f/
-		if chosen_cell.is_flipped
-			puts('you can\'t flag a flipped cell')
-		else
-			chosen_cell.is_flagged = !chosen_cell.is_flagged
+class MineSweeper
+	def start_game
+		# create NxN map
+		@board = N.times.map{ |x| N.times.map{ |y| EmptyCell.new(x, y)}}
+		
+		@cells_left = N*N
+
+		# add X bombs
+		X.times do
+			begin
+				x = rand(1..N) - 1
+				y = rand(1..N) - 1
+			end while @board[y][x].is_a?(MineCell)
+		
+			@board[y][x] = MineCell.new(y, x)
+			(-1..1).each do |i|
+				(-1..1).each do |j|
+					unless (i.zero? && j.zero?) || !([x + i, y + j] & [-1, N]).empty? || @board[y + j][x + i].is_a?(MineCell)
+						@board[y + j][x + i].surrounding_mines += 1
+					end
+				end
+			end
 		end
-		return draw_field true
-	elsif action =~ /[gs]/
+	
+		# start the game
+		puts "New Game!"
+
+		a = Qt::Application.new(ARGV)
+
+		@w = Qt::Widget.new
+		@w.setFixedSize N*40, N*40
+		draw_field
+		@w.show
+
+		a.exec
+	end
+	
+	#returns a bool: true means go to next turn, false means game over
+	def process_input(x, y)
+		chosen_cell = @board[y][x]
+
 		if chosen_cell.is_flipped
 			puts 'this cell has already been flipped, select a different one'
-			 return draw_field true
 		else
 			if chosen_cell.is_a?(MineCell)
 				chosen_cell.is_flipped = true
-				 return draw_field false
+				end_game false
 			else
 				floodfill(x, y)
-				 return draw_field true
 			end
 		end
-	else
-		puts "what does #{action} even mean?"
-		return true
+		return draw_field
 	end
-end
 
-def floodfill(x, y)
-	cell = @board[y][x]
-	return if cell.is_flipped || cell.is_a?(MineCell)
-	cell.is_flipped = true
-	return if cell.surrounding_mines > 0
+	def floodfill(x, y)
+		cell = @board[y][x]
+		return if cell.is_flipped || cell.is_a?(MineCell)
+		cell.is_flipped = true
+		@cells_left -= 1
+		return if cell.surrounding_mines > 0
+		
+		(-1..1).each do |i|
+			(-1..1).each do |j|
+				next if (i.zero? && j.zero?) || !([x + i, y + j] & [-1, N]).empty?
+				floodfill(x + i, y + j)
+			end
+		end
+	end
+
+
+	def draw_field
+		@w.children.each {|c| c.dispose }
+		l = Qt::GridLayout.new(@w) do
+			setColumnStretch 1, 1
+		end
+		@board.each do |x|
+			x.each do |y|
+				l.add_widget(y.to_button, y.x, y.y)
+			end
+		end
+		l.add_widget(Qt::Label.new("Clear this many cells to win: #{@cells_left - X}"), N, 0, 1, N-1)
+		if @cells_left == X
+			end_game true
+		end
+	end
 	
-	(-1..1).each do |i|
-		(-1..1).each do |j|
-			next if (i.zero? && j.zero?) || !([x + i, y + j] & [-1, N]).empty?
-			floodfill(y + j, x + i)
+	def end_game(win)
+		if win
+			puts "winner"
+		else
+			puts "loser"
 		end
 	end
 end
 
-def print_board(reveal_bombs=false)
-	@board.each do |x|
-		x.each do |y|
-			print "#{y.to_str} " 
-		end
-		puts ""
-	end
-	puts ""
-end
-
-#if bool is false, the game is over
-def draw_field(bool)
-	@w.children.each {|c| c.dispose }
-	l = Qt::GridLayout.new(@w) do
-		setColumnStretch 1, 1
-	end
-	@board.each do |x|
-		x.each do |y|
-			b = Qt::PushButton.new(y.to_str) do
-				set_flat true if y.is_flipped
-				resize 40, 40
-			end
-			b.connect(SIGNAL :clicked) do
-				process_input('g', y.y, y.x)
-			end
-			l.add_widget(b, y.x, y.y)
-		end
-	end
-end
-
-# create NxN map
-@board = N.times.map{ |x| N.times.map{ |y| EmptyCell.new(x, y)}}
-
-# add X bombs
-X.times do
-	begin
-		x = rand(1..N) - 1
-		y = rand(1..N) - 1
-	end while @board[y][x].is_a?(MineCell)
-	
-	@board[y][x] = MineCell.new(y, x)
-	(-1..1).each do |i|
-		(-1..1).each do |j|
-			unless (i.zero? && j.zero?) || !([x + i, y + j] & [-1, N]).empty? || @board[y + j][x + i].is_a?(MineCell)
-				@board[y + j][x + i].surrounding_mines += 1
-			end
-		end
-	end
-end
-
-
-# start the game
-puts "New Game!"
-
-a = Qt::Application.new(ARGV)
-
-@w = Qt::Widget.new
-@w.setFixedSize N*40, N*40
-draw_field true
-@w.show
-
-a.exec
+$g = MineSweeper.new
+$g.start_game
